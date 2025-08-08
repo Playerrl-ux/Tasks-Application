4c1ef20b0c1715b95a8a19ab49eab3e0d052b85b
Playerrl-ux <luedybatista96@gmail.com>
2025-08-08 10:25:35 -0300

    refactor: altera senha do usuario admin

--- project/src/main/resources/mongo-init/init-mongo.js
+++ project/src/main/resources/mongo-init/init-mongo.js
@@ -3,5 +3,5 @@ db = db.getSiblingDB('Projeto');
 
 db.Users.insertOne({
     "username": "admin",
-    "password": "$2a$10$k01.KbBBEp3rrPbcRHidEOP0HV6VDyBpdXsZMpp6mhAV96IPWyQ0O",
+    "password": "admin",
 });
